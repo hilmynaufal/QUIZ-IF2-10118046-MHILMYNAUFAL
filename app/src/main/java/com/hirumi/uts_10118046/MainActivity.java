df5cfa912c3a2bb4832ab1eb6e1b2678508638ed
hilmynaufal <hilmyblaze@gmail.com>
2021-05-29 10:45:20 +0700

    second

--- app/src/main/java/com/hirumi/uts_10118046/MainActivity.java
+++ app/src/main/java/com/hirumi/uts_10118046/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    Tanggal pengerjaan = 29-05-2021
+//    Nama: M Hilmy Naufal
+    //NIM: 10118046
+    //Kelas: IF-2
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
